3d3b8b1b5ea6afe4a1a1b27c76a05d4a1aa4ffa7
dotanGit <frenkeldotan1998@gmail.com>
2025-12-12 16:48:28 +0200

    student adapter fix

--- app/src/main/java/com/example/students_dynamic_list/StudentAdapter.kt
+++ app/src/main/java/com/example/students_dynamic_list/StudentAdapter.kt
@@ -3,6 +3,7 @@ package com.example.students_dynamic_list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.students_dynamic_list.databinding.StudentRowLayoutBinding
 import com.example.students_dynamic_list.model.Student
 
 class StudentAdapter (private val students: List<Student>) : RecyclerView.Adapter<StudentRowViewHolder>(){
@@ -10,18 +11,15 @@ class StudentAdapter (private val students: List<Student>) : RecyclerView.Adapte
         parent: ViewGroup,
         viewType: Int
     ): StudentRowViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.student_row_layout, parent, false)
-
-        // Create and return a new ViewHolder instance
-        return StudentRowViewHolder(itemView)
+        val binding = StudentRowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StudentRowViewHolder(binding)
     }
 
     override fun onBindViewHolder(
         holder: StudentRowViewHolder,
         position: Int
     ) {
-        holder.bind(students[position],position)
+        holder.bind(students[position])
     }
 
     override fun getItemCount(): Int {
