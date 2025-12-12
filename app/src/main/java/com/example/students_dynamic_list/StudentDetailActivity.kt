65bcad19c30753e06a697bcaede68502aa4504f6
dotanGit <frenkeldotan1998@gmail.com>
2025-12-12 19:31:43 +0200

    student details layout

--- app/src/main/java/com/example/students_dynamic_list/StudentDetailActivity.kt
+++ app/src/main/java/com/example/students_dynamic_list/StudentDetailActivity.kt
@@ -1,4 +1,20 @@
 package com.example.students_dynamic_list
 
-class StudentDetailActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StudentDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
