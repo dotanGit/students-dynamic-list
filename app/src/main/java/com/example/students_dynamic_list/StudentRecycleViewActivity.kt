42ec7197466c5499888ed0d56e89946104fe973f
dotanGit <frenkeldotan1998@gmail.com>
2025-12-12 14:09:24 +0200

    Add Recycle List Activity and layout

---
+++ app/src/main/java/com/example/students_dynamic_list/StudentRecycleViewActivity.kt
@@ -0,0 +1,22 @@
+package com.example.students_dynamic_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StudentRecycleViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_student_recycle_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+}
